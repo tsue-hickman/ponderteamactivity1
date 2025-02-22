ce10398e69841f145441861980d3c616bab7cad7
Vilate <viknapp22@gmail.com>
2025-02-22 09:06:40 -0700

    modularized functions to checkoutProcess

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,7 +1,21 @@
 //checkout.js
 import { loadHeaderFooter } from "./utils.mjs";
+import checkoutProcess from "./checkoutProcess.mjs";
 
 // Call the productList function when the page loads
 window.addEventListener("DOMContentLoaded", () => {
   loadHeaderFooter();
+  setupFormValidation();
+  checkoutProcess.init("so-cart", "#order-summary");
 });
+
+function setupFormValidation() {
+  const form = document.getElementById("checkout-form");
+
+  form.addEventListener("submit", (event) => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      alert("Please fill out all fields correctly before submitting.");
+    }
+  });
+}
